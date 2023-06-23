3c3b779582f6c6f6724cb72ddb1a11a5dab62a3d
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-06-23 13:39:44 +0300

    Added priority

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
  let form = document.querySelector("form")
  form.addEventListener('submit', (e) =>{
     e.preventDefault(),
-    buildToDoList(e.target.new_task_description.value)
+    buildToDoList(e.target.new_task_description.value,
+       e.target.priority_select.value)
     form.reset()
      
       })      
 });
 
-function buildToDoList(toDo){
+function buildToDoList(toDo, priority){
   let li = document.createElement('li');
   let btn = document.createElement('button');
   btn.addEventListener('click', handleDelete )
@@ -18,6 +19,8 @@ function buildToDoList(toDo){
   li.textContent = `${toDo} `
   btn.textContent= "X";
   li.appendChild(btn);
+  
+  li.classList.add(priority);
 
   document.querySelector('#tasks').appendChild(li)
 }
