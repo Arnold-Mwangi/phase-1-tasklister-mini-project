ede7118eedfe52576b46adf4927b2beac490171f
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-06-23 13:21:35 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+ let form = document.querySelector("form")
+ form.addEventListener('submit', (e) =>{
+    e.preventDefault(),
+    buildToDoList(e.target.new_task_description.value)
+    form.reset()
+     
+      })      
 });
+
+function buildToDoList(toDo){
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete )
+  console.log(btn)
+  li.textContent = `${toDo} `
+  btn.textContent= "X";
+  li.appendChild(btn);
+
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+ }
